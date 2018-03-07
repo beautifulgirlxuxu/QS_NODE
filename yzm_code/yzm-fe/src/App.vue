5e28e1f791209e8fa5e4303c7e265ae33f098fb8
beautifulgirlxuxu <804316947@qq.com>
2018-03-07 20:45:58 +0800

    前端后端实现axios通信

---
+++ yzm_code/yzm-fe/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <ul>
+      <li v-for="(user, index) in users" :key="index">{{user}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      users: []
+    }
+  },  
+  mounted() {
+    this.$http.get('gp/api')
+      .then(data => {
+        console.log(data.data.data)
+        this.users = data.data.data;
+    })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
